d115932adb453c9357d799524526e5f3ecb4faa6
ssf1_kraken <konpqwer@hotmail.com>
2016-12-11 01:44:46 +0300

    globaller dışarıdan alınıyor sabitlerin sembolleri (maalesef) elle giriliyor
    olcumler csv dosyasından sözlük olarak aktarılıyor
    
    tek numune için farklı aktivite hesabı öncesi

--- aktivite_hesabi.py
+++ aktivite_hesabi.py
@@ -1,28 +1,61 @@
-# ------------------------------------------------------------------------------------------------------
-# global degerlerin disaridan alinip degiskenlere atanmasi
+import csv
 
-semboller = []
-degerler = []
+class Aktivite():
+    # ------------------------------------------------------------------------------------------------------
+    # global degerlerin disaridan alinip degiskenlere atanmasi
+    def sabitler():
+        semboller = []
+        degerler = []
 
-with open("sabit_degerler.txt","r") as sabit_degerler:      # sabitlerin oldugu txt dosyasini actik
-    for satir in sabit_degerler.readlines():                # satirlari okuttuk
-        semboldeger, artik = satir.split("\t",1)            # satirlardaki sembol = deger iceren kismi ayirdik
-        sembol, deger = semboldeger.split(' = ',1)            # sembol ve deger kismimi bolduk
-        sembol = str(sembol)                                # sembolleri string yaptik
-        deger = float(deger)                                # sayilari float yaptik
-        semboller.append(sembol)                          # sembol ve degerleri sirayla yeni listeye ekledik
-        degerler.append(deger)
+        with open("sabit_degerler","r") as sabit_degerler:      # sabitlerin oldugu txt dosyasini actik
+            for satir in sabit_degerler.readlines():                # satirlari okuttuk
+                semboldeger, atik = satir.split("\t",1)             # satirlardaki sembol = deger iceren kismi ayirdik
+                sembol, deger = semboldeger.split(' = ')            # sembol ve deger kismimi bolduk
+                sembol = str(sembol)                                # sembolleri string yaptik
+                deger = float(deger)                                # sayilari float yaptik
+                semboller.append(sembol)                            # sembol ve degerleri sirayla yeni listeye ekledik
+                degerler.append(deger)
 
-(S_bg, t_bg, stA_U238, S_st, t_st, m_st, S_stbg, t_stbg, t) = degerler
+        (t_bg, bgU1_Alan, bgU2_Alan, bgU3_Alan, bgCs_Alan, bgTh_Alan, bgK_Alan) = degerler[:7]      #background degerleri
+        (stAU, stATh, stAK, stASoil, m_st) = degerler[7:12]                                               # standart degerleri
+        (t_st, stU1_Alan, stU2_Alan, stU3_Alan, stCs_Alan, stTh_Alan, stK_Alan) = degerler[12:19]     # standart sayimlari
+        (t_stbg, stbgU1_Alan, stbgU2_Alan, stbgU3_Alan, stbgCs_Alan, stbgTh_Alan, stbgK_Alan) = degerler[19:26]  #standart lab. bg degerleri
+
+
+
+    def olcum_degerleri(self):
+        with open("olcumler.csv", "r") as olcumler:
+            reader = csv.DictReader(olcumler)
+            for satir in reader:
+                no = satir['numune']
+                m = float(satir['kütle'])
+                t = float(satir['sayım süresi'])
+                U1 = float(satir['U1'])
+                U2 = float(satir['U2'])
+                U3 = float(satir['U3'])
+                Cs = float(satir['Cs'])
+                Th = float(satir['Th'])
+                K = float(satir['K'])
+                semboller = (U1, U2, U3, Cs, Th, K)
+                print(no, "", m, "", t)
+                for i in semboller:
+                    print(i*2)
+
+print()
+
+
+
+
+"""
 
 # ------------------------------------------------------------------------------------------------------
 # sayim hizlari vs hesaplanmasi
 
-st_sayhizi = S_st/t_st                		#standartın sayim hizi
-stbg_sayhizi = S_stbg/t_stbg          		#standarta ait bg sayim hizi
+st_sayhizi = st_Alan------/t_st----                		#standartın sayim hizi
+stbg_sayhizi = stbg_Alan-----/t_stbg----          		#standarta ait bg sayim hizi
 payda = st_sayhizi - stbg_sayhizi       	#aktivite denkleminin paydasi
 
-bg_sayhizi = float(S_bg) / t_bg
+bg_sayhizi = float(bg_Alan------) / t_bg
 
 class Aktivite():
 
@@ -31,24 +64,24 @@ class Aktivite():
         self.kutle = kutle
         self.alan = alan
         self.sayhizi = float(alan)/t
-        self.A_nm = stA_U238 * (self.sayhizi - bg_sayhizi)/payda * m_st/float(kutle)
+        self.A_nm = stA * (self.sayhizi - bg_sayhizi)/payda * m_st/float(kutle)
 
     def print(self):
-        print("---------------------")
+        print("-" * 20)
         print(self.ornek_no, "numaralı numune değerleri : ")
         print("kütle:", self.kutle, "g", "---","alan=", self.alan)
         print("sayım hızı:", self.sayhizi)
         print("aktivite : ", self.A_nm)
-        print("---------------------")
+        print("-" * 20)
 
-print("-------------------------------------------------------------------")
-print("global değerler")
-print("BG alanı: ", S_bg, "--", "BG sayım süresi", t_bg, "--","BG Sayım hızı : ", bg_sayhizi)
-print("Standart aktivitesi:", stA_U238, "--", " St alani:", S_st, "--", "St sayım suresi", t_st, "--"," St kutlesi:",m_st)
+print("-" * 40)
+print("----- global değerler -----")
+print("BG alanı: ", bg_Alan, "--", "BG sayım süresi", t_bg, "--","BG Sayım hızı : ", bg_sayhizi)
+print("Standart aktivitesi:", stA, "--", " St alani:", st_Alan, "--", "St sayım suresi", t_st, "--"," St kutlesi:",m_st)
 print("Standartın sayım hızı:", st_sayhizi)
-print("Standart BG alanı: ", S_stbg, "--","St BG sayım süresi:", t_stbg)
+print("Standart BG alanı: ", stbg_Alan, "--","St BG sayım süresi:", t_stbg)
 print("Standarta ait BG sayım hızı: ", stbg_sayhizi,"--","St sayım hızı farkı", payda)
-print("-------------------------------------------------------------------")
+print("-" * 40)
 
 # ------------------------------------------------------------------------------------------------------
 # numune giris dongusu
@@ -60,3 +93,7 @@ while (i < 100) :
 
     i += 1
 # ------------------------------------------------------------------------------------------------------
+
+
+
+"""
